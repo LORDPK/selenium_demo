c93543d33d36f4c6becbc051eab2d6bf57a57216
Alvaro <Alvaro@ILEANA-PC>
2021-05-18 00:08:51 -0300

    Se agrega ChromeOptions

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,20 +2,24 @@ package tests;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver _driver;
+    protected ChromeOptions opt;
 
     @BeforeClass
     public void beforeSuite(){
         System.setProperty("webdriver.chrome.driver", "C:\\selenium_drivers\\chromedriver.exe");
+        opt = new ChromeOptions();
+        opt.addArguments("--no-sandbox");
     }
 
     @BeforeMethod
     public void beforeTest(){
-        _driver = new ChromeDriver();
+        _driver = new ChromeDriver(opt);
         _driver.manage().window().maximize();
         _driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
