fcb525f38d5e33ebc9a9121d39db9c16f57d7d1b
Alvaro <Alvaro@ILEANA-PC>
2021-05-17 22:35:51 -0300

    Cambios ejecucion jenkins

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import java.time.Duration;
+import java.time.*;
 
 public class BasePage {
     public WebDriver driver;
